ad86d3256a50b0abfca96688429260f56ab76dd4
Walter Nuccio <wnuccio@hotmail.com>
2020-09-01 16:32:37 +0200

    leap if divisible by 4

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,5 +1,6 @@
 public class LeapYear {
-    public boolean isLeap(int i) {
-        return false;
+    public boolean isLeap(int year) {
+        boolean divisibleBy4 = year % 4 == 0;
+        return divisibleBy4;
     }
 }
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,13 +1,18 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LeapYearTest {
 
     @Test
     void not_leap_if_not_divisible_by_4() {
         assertFalse(new LeapYear().isLeap(1997));
-
     }
+
+    @Test
+    void leap_if_divisible_by_4() {
+        assertTrue(new LeapYear().isLeap(1996));
+    }
+
+
 }
