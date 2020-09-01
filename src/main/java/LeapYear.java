e0024d13275a178d580da6c31b79df1b68b39a64
Walter Nuccio <wnuccio@hotmail.com>
2020-09-01 16:41:48 +0200

    not leap if divisible by 4 but not by 400

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,6 +1,18 @@
 public class LeapYear {
-    public boolean isLeap(int year) {
-        boolean divisibleBy4 = year % 4 == 0;
-        return divisibleBy4;
+    private int year;
+
+    public LeapYear(int year) {
+        this.year = year;
+    }
+
+    private boolean divisibleBy(int divisor) {
+        return year % divisor == 0;
+    }
+
+    public boolean isLeap() {
+        if (divisibleBy(100) && ! divisibleBy(400))
+            return false;
+
+        return divisibleBy(4);
     }
 }
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,17 +1,30 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
 
     @Test
     void not_leap_if_not_divisible_by_4() {
-        assertFalse(new LeapYear().isLeap(1997));
+        assertTrue( ! new LeapYear(1997).isLeap());
     }
 
     @Test
     void leap_if_divisible_by_4() {
-        assertTrue(new LeapYear().isLeap(1996));
+        assertTrue(new LeapYear(1996).isLeap());
+    }
+
+    @Test
+    void not_leap_if_divisible_by_4_but_not_by_400() {
+        assertTrue( ! new LeapYear(1800).isLeap());
+    }
+
+    @Disabled
+    @Test
+    void leap_if_divisible_by_400() {
+        assertTrue(new LeapYear(1600).isLeap());
     }
 
 
